16f4be45196a493405e4bf85698de863916b0eb1
FraChr <empynia@gmail.com>
2026-02-03 13:42:54 +0100

    runner for tests

---
+++ Tests/RunTests.cpp
@@ -0,0 +1,12 @@
+﻿#include "RunTests.h"
+#include <iostream>
+
+void RunTests::ExecuteTests() {
+    int passed = 0;
+    for (auto& test : _tests) {
+        const bool result = test.Func();
+        std::cout << test.testName << '\n';
+        if (result) passed++;
+    }
+    std::cout << "\n" << passed << " / " << std::size(_tests) << " Tests Passed\n";
+}
---
+++ Tests/RunTests.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Test.h"
+#include "TestCase.h"
+
+class RunTests {
+public:
+    void ExecuteTests();
+private:
+    TestCase _tests[3] = {
+        {"Init to Active", Test::TestInitToActive},
+        {"Active to Failure", Test::TestActiveToFailure},
+        {"Emergency Shutdown", Test::TestEmergencyShutdown},
+    };
+};
